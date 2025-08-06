09e414f7b3d1b802811c0c3545bf4abc419aea47
Yukimasa8 <222359572+Yukimasa8@users.noreply.github.com>
2025-08-06 18:00:53 +0900

    feat_Define_blog_post_schema

---
+++ schemaTypes/post.js
@@ -0,0 +1,41 @@
+
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+  ],
+}
